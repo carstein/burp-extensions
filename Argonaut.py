4efa220e30aabd34fc23cb4e807fdea813d04976
Michal Melewski <michal.melewski@gmail.com>
2015-08-12 23:59:48 +0200

    Small fix and cleanup

--- Argonaut.py
+++ Argonaut.py
@@ -4,6 +4,9 @@
 # Process all request parameters
 # and try to find if they are echoed back in response
 
+# Version 0.2
+# Small fix due to NullPointerException
+
 from burp import IBurpExtender
 from burp import IMessageEditorTabFactory
 from burp import IMessageEditorTab
@@ -27,41 +30,44 @@ class BurpExtender(IBurpExtender, IMessageEditorTabFactory):
     self._callbacks = callbacks
     self._helpers = callbacks.getHelpers()
 
-    callbacks.setExtensionName('Argonaut')
+    callbacks.setExtensionName('Argonaut')    
     callbacks.registerMessageEditorTabFactory(self)
-    
+
     return
-  
+
   def createNewInstance(self, controller, editable): 
     return ArgonautTab(self, controller, editable)
 
-    
+
 class ArgonautTab(IMessageEditorTab):
   def __init__(self, extender, controller, editable):
     self._extender = extender
     self._controller = controller
     self._helpers = extender._helpers
-    
+
     # Data container
     self._dataContainer = ArgonautData()
 
     self._argoTable = ArgonautTable(self._dataContainer)
     self._tablePane = JScrollPane(self._argoTable)
-    
+
     return
-    
+
   def getTabCaption(self):
     return "Argonaut"
-    
+
   def getUiComponent(self):
     return self._tablePane
-    
+
   def isEnabled(self, content, isRequest):
     """Enable if parameters were present. Including cookies"""
     if isRequest:
       return False
     else:
-      req = self._helpers.analyzeRequest(self._controller.getRequest())
+      try: #stupid bug, during initialization isEnable is called without context
+        req = self._helpers.analyzeRequest(self._controller.getRequest())
+      except:
+        self.extender._callbacks.issueAlert('isEnabled called without proper context')
 
     params =  req.getParameters()
 
@@ -69,12 +75,13 @@ class ArgonautTab(IMessageEditorTab):
       return False
 
     return True
-  
+
   def setMessage(self, content, isRequest):
     if isRequest:
       return
 
-    # Extract params from pair
+    # Extract params from pair 
+    # TODO: extract depending on request type
     req = self._helpers.analyzeRequest(self._controller.getRequest())
     params =  req.getParameters()
 
@@ -88,10 +95,9 @@ class ArgonautTab(IMessageEditorTab):
     self._dataContainer.fireTableDataChanged()
 
     return
-    
+
   def isModified(self):
     return False
-
 
   def argoParse(self, container, params, body):
     for param in params:
@@ -102,25 +108,26 @@ class ArgonautTab(IMessageEditorTab):
 
       # Search body (TODO: add transformations)
       indexes = [(a.start(), a.end()) for a in list(re.finditer(paramValue, body))]
-      
+
       # Extract
       if indexes:
         for start, end in indexes:
           # TODO: more intelligent snippet
           snippet = body[max(0,start-30):min(end+30, len(body))]
 
-          
+
           container.insertRow(paramValue, 'plain', snippet)
 
-# Stylying
+
 class ArgonautTable(JTable):
   def __init__(self, dataModel):
     self.setModel(dataModel)
     return
 
+
 class ArgonautData(AbstractTableModel):
   _data = []
-  
+
   def reset(self):
     self._data = []
 
@@ -134,8 +141,6 @@ class ArgonautData(AbstractTableModel):
     self._data.append(entry)
 
   def getRowCount(self):
-    print 'row count'
-    print self._data
     return len(self._data)
 
   def getColumnCount(self):
@@ -148,7 +153,7 @@ class ArgonautData(AbstractTableModel):
       return "Transformation"
     if columnIndex == 2:
       return "Snippet"
-    
+
     return ""
 
   def getValueAt(self, rowIndex, columnIndex):
@@ -160,5 +165,5 @@ class ArgonautData(AbstractTableModel):
       return dataEntry['transformation']
     if columnIndex == 2:
       return dataEntry['snippet']
-  
+
     return ""
